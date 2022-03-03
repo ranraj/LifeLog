7b7695e55e33fcf178d214087f6f3eb68e4f1b7a
rishi <rishithumati@gmail.com>
2022-03-03 20:09:15 +0530

    lifelog created

---
+++ src/main/java/com/livelog/myapp/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.livelog.myapp.web.rest;
---
+++ src/main/java/com/livelog/myapp/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.livelog.myapp.web.rest.vm;
