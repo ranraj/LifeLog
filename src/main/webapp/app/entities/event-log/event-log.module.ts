e6e2d1664ce1d7f2c5908b06e659d030f02a91c8
kshan219 <kshan219@gmail.com>
2022-03-18 20:08:27 +0530

    Shan first commit

--- src/main/webapp/app/entities/event-log/event-log.module.ts
+++ src/main/webapp/app/entities/event-log/event-log.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'app/shared/shared.module';
-import { EventLogComponent } from './list/event-log.component';
 import { EventLogDetailComponent } from './detail/event-log-detail.component';
 import { EventLogUpdateComponent } from './update/event-log-update.component';
 import { EventLogDeleteDialogComponent } from './delete/event-log-delete-dialog.component';
@@ -8,7 +7,7 @@ import { EventLogRoutingModule } from './route/event-log-routing.module';
 
 @NgModule({
   imports: [SharedModule, EventLogRoutingModule],
-  declarations: [EventLogComponent, EventLogDetailComponent, EventLogUpdateComponent, EventLogDeleteDialogComponent],
+  declarations: [EventLogDetailComponent, EventLogUpdateComponent, EventLogDeleteDialogComponent],
   entryComponents: [EventLogDeleteDialogComponent],
 })
 export class EventLogModule {}
